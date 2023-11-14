e709630ffa542b68f54567e84d7fad4635c990d6
Sandra-Allies <sandra.allies.fr@gmail.com>
2023-11-14 11:31:14 +0100

    exo chalk

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+const nameUn = 'Nathan';
+const nameDeux = 'Maxence';
+const nameTrois = 'Lou';
+const nameQuatre = 'Christiane';
+
+
+console.log(chalk.red(nameUn), chalk.blue(nameDeux), chalk.green(nameTrois), chalk.yellow(nameQuatre));
